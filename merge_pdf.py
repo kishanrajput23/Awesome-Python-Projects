336d75085bc95e99ae21efc4b9118996942a56f4
aarjav24 <68058926+aarjav24@users.noreply.github.com>
2022-10-01 15:10:28 +0400

    Create function for merging pdfs

---
+++ merge_pdf.py
@@ -0,0 +1,14 @@
+from PyPDF4 import PdfFileReader, PdfFileWriter
+
+def merge_pdfs(paths, output):
+    pdf_writer = PdfFileWriter()
+
+    for path in paths:
+        pdf_reader = PdfFileReader(path)
+        for page in range(pdf_reader.getNumPages()):
+            # Add each page to the writer object
+            pdf_writer.addPage(pdf_reader.getPage(page))
+
+    # Write out the merged PDF
+    with open(output, 'wb') as out:
+        pdf_writer.write(out)
