64b8067557d1df24bb8ad537c52ccf0cd3c5b053
Anisha Shende <91362696+AnishaShende@users.noreply.github.com>
2022-10-02 12:53:55 +0530

    Create Alexa.py
    Created Alexa, a virtual assistant file.

---
+++ Alexa.py
@@ -0,0 +1,57 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def take_command():
+    
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+                #talk(command)
+
+    except:
+        pass
+    return command
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        talk('Current time is ' + time)
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk('Please say the command again.')
+
+while True:
+    run_alexa()
+
+run_alexa()
