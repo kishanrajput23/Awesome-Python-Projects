d0a19681966e64723dfb8fdc6a23a8cc6a49cd9d
Ayush0642 <114876436+Ayush0642@users.noreply.github.com>
2022-10-03 20:18:11 +0530

    Create main.py

---
+++ Stopwatch_Ayush0642/main.py
@@ -0,0 +1,20 @@
+import time
+  
+# define the countdown func.
+def countdown(t):
+    
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+      
+    print('Fire in the hole!!')
+  
+  
+# input time in seconds
+t = input("Enter the time in seconds: ")
+  
+# function call
+countdown(int(t))
