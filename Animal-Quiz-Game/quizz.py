00a504b279e8c34e1df47f946c6bbfef0bb21d11
Big <Big@Coralines-MacBook-Air.local>
2022-10-05 17:33:56 +0700

    revise text display

--- Animal-Quiz-Game/quizz.py
+++ Animal-Quiz-Game/quizz.py
@@ -9,17 +9,17 @@ def check_guess(guess, answer):
             still_guessing = False
         else:
             if attempt < 2:
-                guess = input("Sorry Wrong Answer, try again")
+                guess = input("Sorry Wrong Answer, try again\n:")
             attempt = attempt + 1
     if attempt == 3:
         print("The Correct answer is ",answer )
     
 score = 0
 print("Guess the Animal")
-guess1 = input("Which bear lives at the North Pole? ")
+guess1 = input("Which bear lives at the North Pole?\n:")
 check_guess(guess1, "polar bear")
-guess2 = input("Which is the fastest land animal? ")
+guess2 = input("Which is the fastest land animal?\n:")
 check_guess(guess2, "Cheetah")
-guess3 = input("Which is the larget animal? ")
+guess3 = input("Which is the larget animal?\n:")
 check_guess(guess3, "Blue Whale")
 print("Your Score is "+ str(score))
