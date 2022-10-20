f3015bdc50febef9a37bc80416fd3c06c711c847
Abhinandan Raj <abhinandanraj199@gmail.com>
2022-10-20 19:13:49 +0530

    Create HappyDiwali.py

---
+++ HappyDiwali.py
@@ -0,0 +1,7 @@
+#enter your name
+a = str(input())
+
+print("hello " + a+
+", wishing you a very happy diwali")
+print("May the festival of light\nilluminate your life\nwith joy and happiness")
+print("🪔🪔🎇💥🎆💥🎇✨")
